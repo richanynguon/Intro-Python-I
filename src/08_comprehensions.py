0db01e9b2421811670ba63f3c03f8fb14ea37356
richanynguon <alina.nguon@gmail.com>
2020-02-09 18:13:20 -0500

    complete most of comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -33,9 +33,11 @@ print(y)
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: 23, 21, 5, 8, 6, 4").split(',')
+x = input("Enter comma-separated numbers: 23,21,5,8,6,4").split(',')
 
 # What do you need between the square brackets to make it work?
+
+# ask
 y = [i for i in x if int(i)%2 == 0]
 
 print(y)
